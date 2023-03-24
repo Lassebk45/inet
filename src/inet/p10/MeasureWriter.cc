c695fdd2d434507196639892b2219ab01cd483cf
lasse <lkar18@student.aau.dk>
2023-03-24 15:57:35 +0100

    updated gitignore

---
+++ src/inet/p10/MeasureWriter.cc
@@ -0,0 +1,17 @@
+//
+// Created by lasse on 3/24/23.
+//
+
+#ifndef INET_MEASUREWRITER_H
+#define INET_MEASUREWRITER_H
+
+namespace inet {
+
+class MeasureWriter : cSimpleModule
+{
+
+};
+
+}
+
+#endif //INET_MEASUREWRITER_H
---
+++ src/inet/p10/MeasureWriter.h
@@ -0,0 +1,8 @@
+//
+// Created by lasse on 3/24/23.
+//
+
+#ifndef INET_MEASUREWRITER_H
+#define INET_MEASUREWRITER_H
+
+#endif //INET_MEASUREWRITER_H
