ff9eed6349bd27711c6bd9f55b2ed611021f77fb
lasse <lkar18@student.aau.dk>
2023-03-24 15:59:02 +0100

    support for writing utilization to file during simulation

--- src/inet/p10/MeasureWriter.cc
+++ src/inet/p10/MeasureWriter.cc
@@ -2,16 +2,56 @@
 // Created by lasse on 3/24/23.
 //
 
-#ifndef INET_MEASUREWRITER_H
-#define INET_MEASUREWRITER_H
+#include "inet/p10/MeasureWriter.h"
+#include "inet/p10/json.hpp"
+#include <iomanip>
+#include <string>
+#include <fstream>
 
+namespace omnetpp {
 namespace inet {
 
-class MeasureWriter : cSimpleModule
+Define_Module(MeasureWriter);
+
+void MeasureWriter::receiveSignal(cComponent *source, simsignal_t signalID, double d, cObject *details)
 {
-
-};
-
+    if (dynamic_cast<cDatarateChannel *>(source)) {
+        cDatarateChannel * _source = (cDatarateChannel *) source;
+        std::string srcRouter = _source->getSourceGate()->getOwner()->getFullName();
+        std::string tgtRouter = _source->getSourceGate()->getNextGate()->getOwner()->getFullName();
+        updateUtilization(srcRouter, tgtRouter, d);
+    }
+}
+void MeasureWriter::initialize()
+{
+    getSimulation()->getSystemModule()->subscribe("utilization", this);
+    writeInterval = par("writeInterval");
+    
+    nextWriteTime = SIMTIME_ZERO + writeInterval;
+    scheduleAt(nextWriteTime, writeTrigger);
 }
 
-#endif //INET_MEASUREWRITER_H
+void MeasureWriter::updateUtilization(std::string src, std::string tgt, double utilization)
+{
+    test[src][tgt] = utilization;
+    linkUtilizations[std::make_pair(src, tgt)] = utilization;
+}
+
+void MeasureWriter::handleMessage(cMessage* msg)
+{
+    if (msg == writeTrigger)
+    {
+        double utilizationSnapshotTime = SIMTIME_DBL(nextWriteTime);
+        test["timestamp"] = utilizationSnapshotTime;
+        for (auto it = linkUtilizations.begin(); it != linkUtilizations.end(); ++it)
+        {
+            //printf("%s -> %s: %f\n", it->first.first.c_str(), it->first.second.c_str(), it->second);
+            std::ofstream o("utilization.json");
+            o << std::setw(4) << test << std::endl;
+        }
+        nextWriteTime = nextWriteTime + writeInterval;
+        scheduleAt(nextWriteTime, writeTrigger);
+    }
+}
+}
+}
--- src/inet/p10/MeasureWriter.h
+++ src/inet/p10/MeasureWriter.h
@@ -5,4 +5,34 @@
 #ifndef INET_MEASUREWRITER_H
 #define INET_MEASUREWRITER_H
 
+#include "inet/p10/json.hpp"
+
+namespace omnetpp {
+namespace inet {
+
+class LinkUtilization {
+    public:
+        std::string src = nullptr;
+        std::string tgt = nullptr;
+        double utilization = 0;
+        LinkUtilization(std::string src, std::string tgt, double utilization){this->src = src; this->tgt = tgt; this->utilization = utilization;}
+};
+
+class MeasureWriter : public cSimpleModule, public cListener {
+    private:
+        std::map<std::pair<std::string, std::string>, double> linkUtilizations;
+        nlohmann::json test;
+        simtime_t lastUpdate = SIMTIME_ZERO;
+        simtime_t writeInterval;
+        cMessage* writeTrigger = new cMessage();
+        simtime_t nextWriteTime;
+    protected:
+        virtual void receiveSignal(cComponent *source, simsignal_t signalID, double d, cObject *details) override;
+        virtual void initialize() override;
+        virtual void updateUtilization(std::string src, std::string tgt, double utilization);
+        virtual void handleMessage(cMessage* msg) override;
+};
+
+}
+}
 #endif //INET_MEASUREWRITER_H
